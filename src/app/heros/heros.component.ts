06c4bd681f77b568b5272dcf311d24ffcc72fb78
pigion138138 <kylemoranp@gmail.com>
2018-05-07 20:54:35 -0400

    -Changed the mock heros file type -Added hero selection -Added hero name editing -Added hero styling

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windsotrm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 10, name: 'Mr. Nice'},
+    {id: 11, name: 'Narco'},
+    {id: 12, name: 'Bobasto'},
+    {id: 13, name: 'Kyle'},
+    {id: 14, name: 'Magenta'},
+    {id: 15, name: 'RubberMan'},
+    {id: 16, name: 'Dr IQ'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Magma'},
+    {id: 19, name: 'Tornado'},
+    {id: 20, name: 'Leif'},
+]
