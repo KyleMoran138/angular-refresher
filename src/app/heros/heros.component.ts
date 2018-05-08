cd5d9d81b79d5420c98f0107e6f5866e3e8b7614
pigion138138 <kylemoranp@gmail.com>
2018-05-07 21:56:48 -0400

    -Created hero service -Implemented asynchronous functionality?

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heros',
@@ -10,15 +11,20 @@ import { HEROES } from '../mock-heroes';
 
 export class HerosComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero){
     this.selectedHero = hero;
   }
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
